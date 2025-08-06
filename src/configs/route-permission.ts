8bd69d1edd355fbed264f0494cbc41900fc17c66
Le Dinh Khoi <dinhkhoi.le3@gmail.com>
2025-08-07 01:06:38 +0700

    resolve duplicate code

--- src/configs/route-permission.ts
+++ src/configs/route-permission.ts
@@ -16,142 +16,122 @@ export type RoutePermissionMap = {
     };
 };
 
+const inventoryRequiredRoles = [Role.HeadOfDepartment, Role.PharmacyStaff, Role.WarehouseStaff];
+const managementRequiredRoles = [Role.Administrator, Role.HeadOfDepartment, Role.ITSupport];
+const vaccinationReceptionRequiredRoles = [Role.HeadOfDepartment, Role.Doctor, Role.Nurse, Role.Receptionist];
+const vaccinationRequiredRoles = [Role.Doctor, Role.HeadOfDepartment, Role.Nurse];
+const appointmentsRequiredRoles = [Role.HeadOfDepartment, Role.Doctor, Role.Receptionist];
+const examinationRequiredRoles = [Role.HeadOfDepartment, Role.Doctor, Role.LaboratoryStaff, Role.ImagingTechnician];
+const financeRequiredRoles = [Role.Accountant, Role.Receptionist, Role.Nurse];
+const contractRequiredRoles = [Role.Administrator, Role.Accountant];
+
 export const routePermissions: RoutePermissionMap = {
     // Reception
     "/reception/vaccination": {
         requiredPermissions: [ResourceType.VaccinationReception],
-        requiredRoles: [
-            Role.Administrator,
-            Role.Doctor,
-            Role.Nurse,
-            Role.Receptionist,
-            Role.LaboratoryStaff,
-            Role.ImagingTechnician,
-            Role.Accountant,
-        ],
+        requiredRoles: vaccinationReceptionRequiredRoles,
     },
 
     // Vaccination
     "/vaccination": {
         requiredPermissions: [ResourceType.VaccinationReception],
-        requiredRoles: [
-            Role.Administrator,
-            Role.Doctor,
-            Role.Nurse,
-            Role.Receptionist,
-            Role.LaboratoryStaff,
-            Role.ImagingTechnician,
-        ],
+        requiredRoles: vaccinationRequiredRoles,
     },
     "/vaccination/history": {
         requiredPermissions: [ResourceType.VaccinationReception],
-        requiredRoles: [
-            Role.Administrator,
-            Role.Doctor,
-            Role.Nurse,
-            Role.Receptionist,
-            Role.LaboratoryStaff,
-            Role.ImagingTechnician,
-        ],
+        requiredRoles: vaccinationRequiredRoles,
     },
     "/vaccination/post-injection": {
         requiredPermissions: [ResourceType.VaccinationReception],
-        requiredRoles: [
-            Role.Administrator,
-            Role.Doctor,
-            Role.Nurse,
-            Role.Receptionist,
-            Role.LaboratoryStaff,
-            Role.ImagingTechnician,
-        ],
+        requiredRoles: vaccinationRequiredRoles,
     },
 
     // Appointments
     "/appointments/follow-up": {
         requiredPermissions: [ResourceType.Appointments],
-        requiredRoles: [Role.Administrator, Role.Doctor, Role.Receptionist],
+        requiredRoles: appointmentsRequiredRoles,
     },
 
     // Finance
     "/finance": {
         requiredPermissions: [ResourceType.VaccinationReception],
-        requiredRoles: [Role.Administrator, Role.Accountant],
+        requiredRoles: financeRequiredRoles,
     },
 
     // Examination
     "/examination": {
         requiredPermissions: [ResourceType.VaccinationReception],
-        requiredRoles: [Role.Administrator, Role.Doctor, Role.LaboratoryStaff, Role.ImagingTechnician],
+        requiredRoles: examinationRequiredRoles,
     },
     "/examination/history/patients": {
         requiredPermissions: [ResourceType.VaccinationReception],
-        requiredRoles: [Role.Administrator, Role.Doctor, Role.LaboratoryStaff, Role.ImagingTechnician],
+        requiredRoles: examinationRequiredRoles,
     },
     "/examination/history/patient/:id": {
         requiredPermissions: [ResourceType.VaccinationReception],
-        requiredRoles: [Role.Administrator, Role.Doctor, Role.LaboratoryStaff, Role.ImagingTechnician],
+        requiredRoles: examinationRequiredRoles,
     },
 
     // Pharmacy
     "/pharmacy/import": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
     "/pharmacy/expired-medicine": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
     "/pharmacy/expired-return-form": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
 
     // Medicine Management
     "/medicine/medicine-list": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
     "/medicine/create-medicine": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
     "/medicine/create-medicine-interaction": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
     "/medicine/medicine-interaction-list": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
     "/medicine/medicine-price-list": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
     "/medicine/create-medicine-price": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
 
     // Inventory Management
     "/inventory/limit-stock": {
         requiredPermissions: [ResourceType.Inventory],
-        requiredRoles: [Role.Administrator, Role.PharmacyStaff, Role.WarehouseStaff],
+        requiredRoles: inventoryRequiredRoles,
     },
 
     // Contract Management
     "/contract/supplier": {
         requiredPermissions: [ResourceType.Management],
-        requiredRoles: [Role.Administrator, Role.Accountant],
+        requiredRoles: contractRequiredRoles,
     },
 
     // Management
     "/management/users": {
         requiredPermissions: [ResourceType.Management],
-        requiredRoles: [Role.Administrator, Role.HeadOfDepartment, Role.ITSupport],
+        requiredRoles: managementRequiredRoles,
     },
     "/management/departments": {
         requiredPermissions: [ResourceType.Management],
-        requiredRoles: [Role.Administrator, Role.HeadOfDepartment, Role.ITSupport],
+        requiredRoles: managementRequiredRoles,
     },
     "/management/authorization": {
         requiredPermissions: [ResourceType.Management],
