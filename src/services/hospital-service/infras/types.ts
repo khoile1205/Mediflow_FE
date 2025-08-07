b0b05e642027bed08d1bca6e07ebed37993df664
Haiviet511 <Haiviet411@gmail.com>
2025-08-07 09:48:08 +0700

    Doing interation api for hospital service

--- src/services/hospital-service/infras/types.ts
+++ src/services/hospital-service/infras/types.ts
@@ -42,35 +42,6 @@ export interface RemoveServicesFromGroupRequest {
     serviceGroupId: number;
     serviceIds: number[];
 }
-
-export interface HospitalService {
-    id: number;
-    serviceCode: string;
-    serviceName: string;
-    unitPrice: number;
-    departmentId: number;
-    createdAt?: string;
-    lastUpdatedAt?: string;
-    serviceGroups?: { id: number; groupName: string }[];
-    diseaseGroups?: { id: number; groupName: string; description?: string }[];
-    examinationService?: number | null;
-    serviceTestParameters?: {
-        id: number;
-        serviceId: number;
-        parameterName: string;
-        unit: string;
-        standardValue: string;
-        equipmentName: string;
-        specimenType: string;
-        isSuspended: boolean;
-        isCancelled: boolean;
-        createdAt?: string;
-        createdBy?: number;
-        lastUpdatedAt?: string;
-        lastUpdatedBy?: number;
-    }[];
-}
-
 export interface PaginationResult<T> {
     data: T[];
     pageIndex: number;
